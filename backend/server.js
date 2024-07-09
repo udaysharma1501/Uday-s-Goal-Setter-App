e40dc3c18ac71f5015c32ca8a24c8b1d938ac904
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-10 03:09:11 +0530

    basic crud operations

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
