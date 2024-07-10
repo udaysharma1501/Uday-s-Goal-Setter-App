03823f567f399dec3270baeaa03a618e5a68f684
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-10 05:57:09 +0530

    final commit - CRUD up and running

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -24,11 +24,32 @@ const setGoals = asyncHandler(async (req, res) => {
 });
 
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(! goal){
+        res.status(400);
+        throw new Error('goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
+
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+      res.status(400);
+      throw new Error("goal not found");
+    }
+
+    // await goal.remove();
+    await Goal.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
