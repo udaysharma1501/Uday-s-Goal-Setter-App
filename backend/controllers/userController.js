80ef2ab2cf7e53906b5aaf4d5bc1a9619b420fce
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-10 17:23:05 +0530

    JWTA3 - protecting routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,16 +21,77 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("user already exists");
     }
 
-  res.json({ message: "register user" });
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    if(user){
+        // something was created and everything is okay
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    }
+    else{
+        res.status(400);
+        throw new Error("invalid user data");
+    }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+    
+    const {email, password} = req.body;
+
+    // matching email and password against the saved DB to retrieve the name
+    const user = await User.findOne({email});
+
+    // comparing the entered password and hashed password using bcrypt method - compare 
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+          _id: user.id,
+          name: user.name,
+          email: user.email,
+          // the token issued here can be verified at jwt.io          
+          token: generateToken(user._id)
+        });
+    }
+    else{
+
+        res.status(400);
+        throw new Error("invalid credentials");
+    }
+
   res.json({ message: "login user" });
 });
 
+// private access
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "user data display" });
+
+    const {_id, name, email} =  await User.findById(req.user.id);
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
+
 });
+
+// generate JWT - this signs a new token with the id passed in and the secret used which would expire in 30 days
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+}
 
 module.exports = {
   registerUser,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {registerUser, loginUser, getMe} = require('../controllers/userController');
 
+const {protect} = require('../middlewear/authMiddelwear');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+// router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 
 
