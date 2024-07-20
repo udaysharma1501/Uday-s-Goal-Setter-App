a3fdd961a08a2f48f0deab3be2af5b375b4366ca
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-20 16:50:45 +0530

    final test commit 5 - pre deployment

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,15 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 // serve frontend
-if(process.env.NODE_ENV === 'production'){
-    
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 // overrides the default express errorHandler
