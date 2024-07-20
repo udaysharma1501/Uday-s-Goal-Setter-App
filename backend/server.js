cb70d71941e89783619bcb73be80b5bc613f7162
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-20 11:27:02 +0530

    test commit 2

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const {errorHandler} = require('./middlewear/errorMiddlewear');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+const path = require('path');
+
 connectDB();
 
 const app = express();
@@ -15,6 +17,14 @@ app.use(express.urlencoded({extended:false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// serve frontend
+if(process.env.NODE_ENV === 'production'){
+    
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
+}
 
 // overrides the default express errorHandler
 app.use(errorHandler);
